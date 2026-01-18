c8768894a4374fa53bb9181debe0135ed2f9b8d6
smgzach <zm989392@gmail.com>
2026-01-18 15:53:10 -0500

    init commit

---
+++ backend/src/test/java/com/zachprogramming/aifortuneteller/AiFortuneTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachprogramming.aifortuneteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFortuneTellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
