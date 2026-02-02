ba45b5057bf40058618eaa5f1f97a286a5d56bb6
smgzach <zm989392@gmail.com>
2026-02-02 11:54:01 -0500

    security is broken
    - added user controller with register method

--- backend/src/main/java/com/zachprogramming/aifortuneteller/Controllers/UserController.java
+++ backend/src/main/java/com/zachprogramming/aifortuneteller/Controllers/UserController.java
@@ -9,8 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/fortunes")
-public class FortuneController
+public class UserController
 {
+    private UserService userService;
+
+    public  UserController(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> createUser(@RequestBody User user)
+    {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        User newUser = userService.createNewUser(username, password);
+        return ResponseEntity.ok(newUser);
+    }
 
 }
